c8349bd1e92582d1db228f1a61c4deb0c00350bd
yourUsername <yourEmail@domain.com>
2020-02-16 17:31:01 -0500

    kk

---
+++ src/StringInteger.java
@@ -0,0 +1,24 @@
+public class StringInteger {
+public static void main(String[]args) {
+
+
+   String s = "102" ;
+   System.out.print( Integer.parseInt(s)+ 6 * 6);
+ //  int n = Integer.parseInt( "3" );
+  // System.out.println( n+8 );
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+}
